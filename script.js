a842ec397b80a78fe346925a3a40f75b39f5db65
ThanhMOOC <tacthahn.mooc-ext@vinci-autoroutes.com>
2025-04-24 17:01:47 +0200

    add img folder add about, portfolio, page

--- script.js
+++ script.js
@@ -7,3 +7,15 @@ links.forEach(link => {
     document.querySelector(this.getAttribute('href')).scrollIntoView({ behavior: 'smooth' });
   });
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+  const contactBtn = document.getElementById('nav-contact');
+  const contactSection = document.getElementById('contact');
+
+  contactBtn.addEventListener('click', function (e) {
+    e.preventDefault(); // chặn hành vi mặc định
+    contactSection.classList.remove('highlight');
+    void contactSection.offsetWidth; // trigger reflow
+    contactSection.classList.add('highlight');
+  });
+});
